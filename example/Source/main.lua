fe618028b492dfff35ee6905101ce6c8dfa75494
foxblock <foxblock+github@gmail.com>
2024-02-19 22:44:20 +0100

    Add sweep-and-prune on one axis (done in Lua)
    Unfortunately the results are rather disappointing
    For 4 balls it's 50-100% slower, for 20 it's still 20% slower (measured in comp time / frame in ms)
    Even though 80-95% of collision checks are avoided (number is drawn under ball count)
    
    Additionally just the change from SOA to AOS of the data incurs a 5% performance penalty

--- example/Source/main.lua
+++ example/Source/main.lua
@@ -8,19 +8,26 @@ local v2d <const> = collision.vector2D
 local poly <const> = collision.polygon
 local coll <const> = collision
 
-
-local positions = {
-    v2d.new(50,50),
-    v2d.new(260,50),
-    v2d.new(260,150),
-    v2d.new(50,150),
+-- NOTE: Just changing the structure from SOA-style (separate position and vector arrays)
+-- to AOS (bundled unit objects) is about 5% slower on its own
+local units = {
+    { p = v2d.new(50,50), v = v2d.new(3,1) },
+    { p = v2d.new(260,50), v = v2d.new(-2,2) },
+    { p = v2d.new(260,150), v = v2d.new(1,1) },
+    { p = v2d.new(50,150), v = v2d.new(3,-2) },
 }
-local velocities = {
-    v2d.new(3,1),
-    v2d.new(-2,2),
-    v2d.new(1,1),
-    v2d.new(3,-2),
-}
+-- local positions = {
+--     v2d.new(50,50),
+--     v2d.new(260,50),
+--     v2d.new(260,150),
+--     v2d.new(50,150),
+-- }
+-- local velocities = {
+--     v2d.new(3,1),
+--     v2d.new(-2,2),
+--     v2d.new(1,1),
+--     v2d.new(3,-2),
+-- }
 local radius = 10
 local bigPoly = poly.new(
     120, 120,
@@ -43,17 +50,25 @@ local function drawV2DListAsPoly(list)
     end
 end
 
+local function compUnit(a, b)
+    -- doing the comparison in Lua (a.p.x < b.p.x) is slower by about 10-20%
+    return a.p < b.p
+end
+
+local checkCount = 0
+
 local function render()
     gfx.clear(gfx.kColorWhite)
 
-    for i = 1, #positions do
-        gfx.drawCircleAtPoint(positions[i], radius)
+    for i = 1, #units do
+        gfx.drawCircleAtPoint(units[i].p, radius)
     end
     drawV2DListAsPoly(bigPoly)
     -- gfx.drawCircleAtPoint(polyMiddle, polyRadius)
 
     playdate.drawFPS(380,2)
-    gfx.drawText(string.format("%d", #positions), 380, 17)
+    gfx.drawText(string.format("%d", #units), 380, 17)
+    gfx.drawText(string.format("%d", checkCount), 370, 32)
 end
 
 local time = 0
@@ -66,45 +81,53 @@ local depth = 0
 function playdate.update()
     time = playdate.getElapsedTime()
 
-    for i=1, #positions do
-        if ((positions[i].x < radius) and (velocities[i].x < 0)) or
-                ((positions[i].x >= screen.getWidth() - radius) and (velocities[i].x > 0)) then
-            velocities[i].x = -velocities[i].x
+    for i=1, #units do
+        if ((units[i].p.x < radius) and (units[i].v.x < 0)) or
+                ((units[i].p.x >= screen.getWidth() - radius) and (units[i].v.x > 0)) then
+            units[i].v.x = -units[i].v.x
         end
-        if ((positions[i].y < radius) and (velocities[i].y < 0)) or
-                ((positions[i].y >= screen.getHeight() - radius) and (velocities[i].y > 0)) then
-            velocities[i].y = -velocities[i].y
+        if ((units[i].p.y < radius) and (units[i].v.y < 0)) or
+                ((units[i].p.y >= screen.getHeight() - radius) and (units[i].v.y > 0)) then
+            units[i].v.y = -units[i].v.y
         end
 
-        positions[i]:addScaled(velocities[i], 1)
+        units[i].p:addScaled(units[i].v, 1)
     end
 
-    for i=1, #positions do
-        for k=i+1, #positions do
-            local collides = coll.circleCircle_check(positions[i], radius, positions[k], radius)
+    -- NOTE: Unfortunately the sort in Lua seems to be more expensive than doing the collision checks in C
+    -- for 4 balls it's 50-100% slower, for 20 it's about 20% slower
+    table.sort(units, compUnit)
+    checkCount = 0
+
+    for i=1, #units do
+        for k=i+1, #units do
+            if units[k].p.x - radius > units[i].p.x + radius then break end
+
+            checkCount = checkCount + 1
+            local collides = coll.circleCircle_check(units[i].p, radius, units[k].p, radius)
             if not collides then goto continue_inner end
 
-            colNormal, depth = coll.circleCircle(positions[i], radius, positions[k], radius)
-            positions[i]:addScaled(colNormal, -depth / 2)
-            positions[k]:addScaled(colNormal, depth / 2)
+            colNormal, depth = coll.circleCircle(units[i].p, radius, units[k].p, radius)
+            units[i].p:addScaled(colNormal, -depth / 2)
+            units[k].p:addScaled(colNormal, depth / 2)
 
-            local velDiff = v2d.new(velocities[i].x - velocities[k].x, velocities[i].y - velocities[k].y)
+            local velDiff = v2d.new(units[i].v.x - units[k].v.x, units[i].v.y - units[k].v.y)
             local relSpeed = colNormal:dotProduct(velDiff)
-            velocities[i]:addScaled(colNormal, -relSpeed)
-            velocities[k]:addScaled(colNormal, relSpeed)
+            units[i].v:addScaled(colNormal, -relSpeed)
+            units[k].v:addScaled(colNormal, relSpeed)
 
             ::continue_inner::
         end
 
-        local collides = coll.circleCircle_check(positions[i], radius, polyMiddle, polyRadius)
+        local collides = coll.circleCircle_check(units[i].p, radius, polyMiddle, polyRadius)
         if not collides then goto continue end
 
-        colNormal, depth = coll.circlePoly(positions[i], radius, bigPoly)
+        colNormal, depth = coll.circlePoly(units[i].p, radius, bigPoly)
         if colNormal ~= nil then
-            positions[i]:addScaled(colNormal, -depth)
+            units[i].p:addScaled(colNormal, -depth)
 
-            local dp = colNormal:dotProduct(velocities[i])
-            velocities[i]:addScaled(colNormal, -2 * dp)
+            local dp = colNormal:dotProduct(units[i].v)
+            units[i].v:addScaled(colNormal, -2 * dp)
         end
 
         ::continue::
@@ -129,15 +152,13 @@ function playdate.AButtonUp()
         collides = coll.circleCircle_check(pos, radius, polyMiddle, polyRadius)
     end
 
-    table.insert(positions, pos)
     local vel = v2d.new(math.random() * 2 + 1, math.random() * 2 + 1)
-    table.insert(velocities, vel)
+    table.insert(units, {p = pos, v = vel})
 end
 
 function playdate.BButtonUp()
-    local count = #positions
-    for i=1, count do positions[i]=nil end
-    for i=1, count do velocities[i]=nil end
+    local count = #units
+    for i=1, count do units[i]=nil end
 end
 
 ---
