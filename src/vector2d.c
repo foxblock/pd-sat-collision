788bedf3624a35210e9f1835196eed845e9528d4
foxblock <foxblock+github@gmail.com>
2024-02-19 22:31:29 +0100

    Add comparison operators to vector2d
    These do not make sense in a geometrical way, but are useful for implementing sweep-and-prune style algorithms

--- src/vector2d.c
+++ src/vector2d.c
@@ -11,8 +11,8 @@ static inline float square(float v)
 void vector2D_normalize(Vector2D *v)
 {
     float len = vector2D_length(*v);
-	if (len == 0)
-		return;
+    if (len == 0)
+        return;
 
     v->x /= len;
     v->y /= len;
@@ -35,8 +35,8 @@ void vector2D_rightNormal(Vector2D *target, Vector2D src)
 void vector2D_dirNormalized(Vector2D *target, Vector2D pa, Vector2D pb)
 {
     float len = sqrtf(square(pa.x - pb.x) + square(pa.y - pb.y));
-	if (len == 0)
-		len = 1.0f;
+    if (len == 0)
+        len = 1.0f;
     target->x = (pa.x - pb.x) / len;
     target->y = (pa.y - pb.y) / len;
 }
@@ -64,75 +64,75 @@ float vector2D_dotProduct(Vector2D a, Vector2D b)
 
 size_t vector2D_print(char* dest, size_t len, Vector2D v)
 {
-	//int bytesWritten = snprintf(dest, len, "(%.2f, %.2f)", v.x, v.y);
-	//if (len < bytesWritten)
-	//	return len;
-	//if (bytesWritten < 0)
-	//	return 0;
-	//return bytesWritten;
-	return 0;
+    //int bytesWritten = snprintf(dest, len, "(%.2f, %.2f)", v.x, v.y);
+    //if (len < bytesWritten)
+    //	return len;
+    //if (bytesWritten < 0)
+    //	return 0;
+    //return bytesWritten;
+    return 0;
 }
 
 // --- LUA HOOKS ---
 
 static int lua_vector2d_new(lua_State *L)
 {
-	Vector2D* v = pd->system->realloc(NULL, sizeof(Vector2D));
+    Vector2D* v = pd->system->realloc(NULL, sizeof(Vector2D));
 
-	if (pd->lua->getArgCount() == 1)
-	{
-		Vector2D* other = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
-		*v = *other;
-	}
-	else
-	{
-		v->x = pd->lua->getArgFloat(1);
-		v->y = pd->lua->getArgFloat(2);
-	}
+    if (pd->lua->getArgCount() == 1)
+    {
+        Vector2D* other = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
+        *v = *other;
+    }
+    else
+    {
+        v->x = pd->lua->getArgFloat(1);
+        v->y = pd->lua->getArgFloat(2);
+    }
 
-	pd->lua->pushObject(v, VECTOR_TYPE_NAME, 0);
-	return 1;
+    pd->lua->pushObject(v, VECTOR_TYPE_NAME, 0);
+    return 1;
 }
 
 static int lua_vector2d_free(lua_State *L)
 {
-	Vector2D* v = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
-	if (v != NULL)
-		pd->system->realloc(v, 0);
-	return 0;
+    Vector2D* v = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
+    if (v != NULL)
+        pd->system->realloc(v, 0);
+    return 0;
 }
 
 static int lua_vector2d_index(lua_State *L)
 {
-	// https://sdk.play.date/2.3.1/Inside%20Playdate%20with%20C.html#f-lua.indexMetatable
-	// Maybe we don't need this?
-	if (pd->lua->indexMetatable())
-		return 1;
+    // https://sdk.play.date/2.3.1/Inside%20Playdate%20with%20C.html#f-lua.indexMetatable
+    // Maybe we don't need this?
+    if (pd->lua->indexMetatable())
+        return 1;
 
-	Vector2D* v = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
-	const char* arg = pd->lua->getArgString(2);
-	
-	if (strcmp(arg, "x") == 0)
-		pd->lua->pushFloat(v->x);
-	else if (strcmp(arg, "y") == 0)
-		pd->lua->pushFloat(v->y);
-	else
-		pd->lua->pushNil();
-	
-	return 1;
+    Vector2D* v = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
+    const char* arg = pd->lua->getArgString(2);
+    
+    if (strcmp(arg, "x") == 0)
+        pd->lua->pushFloat(v->x);
+    else if (strcmp(arg, "y") == 0)
+        pd->lua->pushFloat(v->y);
+    else
+        pd->lua->pushNil();
+    
+    return 1;
 }
 
 static int lua_vector2d_newindex(lua_State *L)
 {
-	Vector2D* v = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
-	const char* arg = pd->lua->getArgString(2);
-	
-	if (strcmp(arg, "x") == 0)
-		v->x = pd->lua->getArgFloat(3);
-	else if (strcmp(arg, "y") == 0)
-		v->y = pd->lua->getArgFloat(3);
-	
-	return 0;
+    Vector2D* v = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
+    const char* arg = pd->lua->getArgString(2);
+    
+    if (strcmp(arg, "x") == 0)
+        v->x = pd->lua->getArgFloat(3);
+    else if (strcmp(arg, "y") == 0)
+        v->y = pd->lua->getArgFloat(3);
+    
+    return 0;
 }
 
 static int lua_vector2d_print(lua_State *L)
@@ -143,84 +143,114 @@ static int lua_vector2d_print(lua_State *L)
  //   vector2D_print(result, sizeof(result), *v);
 
  //   pd->lua->pushString(result);
-	//return 1;
-	return 0;
+    //return 1;
+    return 0;
+}
+
+static int lua_vector2d_op_lt_x(lua_State *L)
+{
+    Vector2D* a = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
+    Vector2D* b = pd->lua->getArgObject(2, VECTOR_TYPE_NAME, NULL);
+
+    int res = a->x < b->x;
+
+    pd->lua->pushBool(res);
+    return 1;
+}
+
+static int lua_vector2d_op_lt_y(lua_State *L)
+{
+    Vector2D* a = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
+    Vector2D* b = pd->lua->getArgObject(2, VECTOR_TYPE_NAME, NULL);
+
+    int res = a->y < b->y;
+
+    pd->lua->pushBool(res);
+    return 1;
+}
+
+static int lua_vector2d_op_lt(lua_State* L)
+{
+    return lua_vector2d_op_lt_x(L);
 }
 
 static int lua_vector2d_unpack(lua_State *L)
 {
-	Vector2D* v = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
+    Vector2D* v = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
 
-	pd->lua->pushFloat(v->x);
-	pd->lua->pushFloat(v->y);
-	return 2;
+    pd->lua->pushFloat(v->x);
+    pd->lua->pushFloat(v->y);
+    return 2;
 }
 
 static int lua_vector2d_addScaled(lua_State *L)
 {
-	int argc = pd->lua->getArgCount();
-	if ((argc - 1) % 2 != 0)
-	{
-		pd->system->error("%s:%i: Invalid arguments for vector2d:addScaled()! Must be "
-			"alternating list of vector2d and scaling factors (float).",
-			__FILE__, __LINE__);
-		return 0;
-	}
+    int argc = pd->lua->getArgCount();
+    if ((argc - 1) % 2 != 0)
+    {
+        pd->system->error("%s:%i: Invalid arguments for vector2d:addScaled()! Must be "
+            "alternating list of vector2d and scaling factors (float).",
+            __FILE__, __LINE__);
+        return 0;
+    }
 
-	Vector2D* v = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
-	for (int i = 2; i < argc; i += 2)
-	{
-		Vector2D* other = pd->lua->getArgObject(i, VECTOR_TYPE_NAME, NULL);
-		float scale = pd->lua->getArgFloat(i + 1);
+    Vector2D* v = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
+    for (int i = 2; i < argc; i += 2)
+    {
+        Vector2D* other = pd->lua->getArgObject(i, VECTOR_TYPE_NAME, NULL);
+        float scale = pd->lua->getArgFloat(i + 1);
 
-		vector2D_addVecScaled(v, *other, scale);
-	}
+        vector2D_addVecScaled(v, *other, scale);
+    }
 
-	return 0;
+    return 0;
 }
 
 static int lua_vector2d_normalize(lua_State *L)
 {
-	Vector2D* v = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
+    Vector2D* v = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
 
-	vector2D_normalize(v);
+    vector2D_normalize(v);
 
-	return 0;
+    return 0;
 }
 
 static int lua_vector2d_dotProduct(lua_State *L)
 {
-	Vector2D* a = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
-	Vector2D* b = pd->lua->getArgObject(2, VECTOR_TYPE_NAME, NULL);
+    Vector2D* a = pd->lua->getArgObject(1, VECTOR_TYPE_NAME, NULL);
+    Vector2D* b = pd->lua->getArgObject(2, VECTOR_TYPE_NAME, NULL);
 
-	float res = vector2D_dotProduct(*a, *b);
+    float res = vector2D_dotProduct(*a, *b);
 
-	pd->lua->pushFloat(res);
-	return 1;
+    pd->lua->pushFloat(res);
+    return 1;
 }
 
 // --- END LUA HOOKS ---
 
 static const lua_reg vector2Dlib[] =
 {
-	{ "new", 		lua_vector2d_new },
-	{ "__gc",		lua_vector2d_free },
-	{ "__index", 	lua_vector2d_index },
-	{ "__newindex",	lua_vector2d_newindex },
-	{ "__tostring",	lua_vector2d_print },
-	{ "unpack",		lua_vector2d_unpack },
-	{ "addScaled",	lua_vector2d_addScaled },
-	{ "normalize",	lua_vector2d_normalize },
-	{ "dotProduct",	lua_vector2d_dotProduct },
-	{ NULL, NULL }
+    { "new", 		lua_vector2d_new },
+    { "__gc",		lua_vector2d_free },
+    { "__index", 	lua_vector2d_index },
+    { "__newindex",	lua_vector2d_newindex },
+    { "__tostring",	lua_vector2d_print },
+    { "__lt",		lua_vector2d_op_lt },
+    { "ltX",		lua_vector2d_op_lt_x },
+    { "ltY",		lua_vector2d_op_lt_y },
+    { "unpack",		lua_vector2d_unpack },
+    { "addScaled",	lua_vector2d_addScaled },
+    { "normalize",	lua_vector2d_normalize },
+    { "dotProduct",	lua_vector2d_dotProduct },
+    { NULL, NULL }
 };
 
 void registerVector2D(PlaydateAPI* playdate)
 {
-	pd = playdate;
-	
-	const char* err;
-	
-	if (!pd->lua->registerClass(VECTOR_TYPE_NAME, vector2Dlib, NULL, 0, &err))
-		pd->system->error("%s:%i: registerClass failed, %s", __FILE__, __LINE__, err);
+    pd = playdate;
+    
+    const char* err;
+    
+    if (!pd->lua->registerClass(VECTOR_TYPE_NAME, vector2Dlib, NULL, 0, &err))
+        pd->system->error("%s:%i: registerClass failed, %s", __FILE__, __LINE__, err);
 }
